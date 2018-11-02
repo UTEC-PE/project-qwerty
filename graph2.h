d02f6e424084410ac750483eb31b15fafdbc5c92
Alessia Yi <alessia.yi@utec.edu.pe>
2018-11-02 12:11:54 -0500

    Segunda parte

---
+++ graph2.h
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+#include "node.h"
+#include "edge.h"
+
+using namespace std;
+
+class Traits2 {
+public:
+	typedef char N;
+	typedef int E;
+};
+
+template <typename Tr>
+class Graph2 {
+public:
+	typedef Graph2<Tr> self;
+	typedef Node<self> node;
+	typedef Edge<self> edge;
+	typedef vector<node*> NodeSeq;
+	typedef list<edge*> EdgeSeq;
+	typedef typename Tr::N N;
+	typedef typename Tr::E E;
+	typedef typename NodeSeq::iterator NodeIte;
+	typedef typename EdgeSeq::iterator EdgeIte;
+
+private:
+	NodeSeq nodes;
+	NodeIte ni;
+	EdgeIte ei;
+
+public:
+	Graph2():nodes(0){}
+
+	void pqq(){
+		cout << "HOLA";
+	}
+
+	void insertNode(N name){
+		bool exists=false;
+		for (ni=nodes.begin(); ni!=nodes.end(); ni++){
+			if (name==(*ni) -> getData()){
+				exists=true;
+			}
+		}
+		if (!exists){
+			node* nodo=new node(name);
+			nodes.push_back(nodo);
+		}
+	}
+
+	void insertEdge(N nodo1, N nodo2, E value, bool dir){
+		bool exists1=false;
+		bool exists2=false;
+		node* tmp=nullptr;
+		node* tmp2=nullptr;
+		for (ni=nodes.begin(); ni!=nodes.end(); ni++){
+			if (nodo1==(*ni) -> getData()){
+				tmp=*ni;
+				exists1=true;
+			}
+			if (nodo2==(*ni) -> getData()){
+				tmp2=*ni;
+				exists2=true;
+			}
+		}
+		if (exists1 && exists2){
+			edge *arista1=new edge(tmp, tmp2, value, dir);
+			tmp -> edges.push_back(arista1);
+			if (!dir){
+				edge *arista2=new edge(tmp2, tmp, value, dir);
+				tmp2 -> edges.push_back(arista2);
+			}
+		}
+	}
+
+	void removeNode(N name){
+		bool exists=false;
+		for (ni=nodes.begin(); ni!=nodes.end(); ni++){
+			if (name==(*ni) -> getData()){
+				node* tmp=(*ni);
+				delete tmp;
+				nodes.erase(ni);
+				exists=true;
+				break;
+			}
+		}
+		for (ni=nodes.begin(); ni!=nodes.end(); ni++){
+			for (ei=(*ni) -> edges.begin(); ei!=(*ni) -> edges.end(); ei++) {
+				if ((*ei)->nodes[1]->getData() == name){
+					(*ni) -> edges.remove(*ei);
+					break;
+				}
+			}
+		}
+		if (!exists){
+			cout << "No existe node ";
+		}
+	}
+
+	void removeEdge(N nodo1, N nodo2){
+		bool exists=false;
+		for(ni=nodes.begin(); ni!=nodes.end(); ni++){
+			if (nodo1==(*ni) -> getData()){
+				for (ei=(*ni) -> edges.begin(); ei!=(*ni) -> edges.end(); ei++){
+					if (((*ei) -> nodes[1]) -> getData()==nodo2){
+						(*ni) -> edges.remove(*ei);
+						exists=true;
+						break;
+					}
+				}
+			}
+			if (nodo2==(*ni) -> getData()){
+				for (ei=(*ni) -> edges.begin(); ei!=(*ni) -> edges.end(); ei++){
+					if (((*ei) -> nodes[1]) -> getData()==nodo1){
+						(*ni) -> edges.remove(*ei);
+						break;
+					}
+				}
+			}
+		}
+		if (!exists){
+			cout << "No existe edge ";
+		}
+	}
+
+	void imprimeNodes(){
+		cout << "Nodes: ";
+		for (ni=nodes.begin(); ni!=nodes.end(); ni++){
+			cout << (*ni) -> getData() << " ";
+		}
+		cout << endl;
+	}
+
+	void imprimeEdges(N nodo){
+		cout << "Edges de " << nodo << ": ";
+		for (ni=nodes.begin(); ni!=nodes.end(); ni++){
+			if (nodo==(*ni) -> getData()){
+				cout << (*ni) -> edges.size() << " ";
+			}
+		}
+		cout << endl;
+	}
+
+	void print(){
+    cout << "Vertices / Aristas" << endl;
+		for(ni = nodes.begin(); ni != nodes.end(); ni++){
+			cout << (*ni)->getData() << ' ';
+			for(ei = (*ni)->edges.begin(); ei != (*ni)->edges.end(); ei++){
+				cout << (*ei)->getData() << ' ';
+			}
+			cout << endl;
+		}
+	}
+
+	~Graph2(){
+		vector<node*>().swap(nodes);
+	}
+};
+
+typedef Graph2<Traits2> graph2;
--- main.cpp
+++ main.cpp
@@ -2,28 +2,15 @@
 #include <iostream>
 
 #include "graph.h"
+#include "graph2.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-  graph Graph;
+  graph2 Graph;
 
-  Graph.insertNode('A');
-  Graph.insertNode('C');
-  Graph.insertNode('D');
-  Graph.insertNode('E');
-  Graph.insertEdge('A', 'C', 3, 1);
-  Graph.insertEdge('C', 'E', 2, 1);
-  Graph.insertEdge('D', 'E', 9, 1);
-  Graph.insertEdge('E', 'A', 5, 1);
-  Graph.insertEdge('A', 'D', 5, 1);
-  Graph.print();
-
-  cout << "--------------" << endl;
-  cout << Graph.fuerteConexo();
-
-
+  Graph.pqq();
 
   return EXIT_SUCCESS;
 }
